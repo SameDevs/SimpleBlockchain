bb98abe52c08a0609dba0e3843c9f0d6b24528c1
Giovanni Liboni <giovanni@liboni.net>
2017-12-29 19:01:01 +0100

    Added blocks route

--- blockchain_py/src/main.py
+++ blockchain_py/src/main.py
@@ -2,18 +2,25 @@ import os
 import hashlib
 import requests
 import time
+import json
 
 from block import Block
+from blockchain import Blockchain
 
 from flask import Flask
 
 app = Flask(__name__)
+
+blockchain = Blockchain()
 
 @app.route('/')
 def hello():
     b = Block(0, time.ctime(), "Genesis Block", "0")
     return "%s, %s, %s, %s, %s" % (b.index, b.timestamp, b.data, b.hash, b.previous_hash)
 
+@app.route('/blocks')
+def get_blocks():
+    return json.dumps(blockchain.toDict())
 
 if __name__ == "__main__":
     SERVICE_PORT = int(os.getenv("PORT", 5000))
