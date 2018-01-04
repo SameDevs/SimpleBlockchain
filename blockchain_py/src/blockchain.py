1128230260482f988884beadd491acb2fa2980d7
Giovanni Liboni <giovanni@liboni.net>
2018-01-04 16:30:20 +0100

    Added new endpoints: transactions and mine

--- blockchain_py/src/blockchain.py
+++ blockchain_py/src/blockchain.py
@@ -1,17 +1,49 @@
 import datetime as date
 from block import Block
 
-def create_genesis_block():
-  return Block(0, date.datetime.now(), "Genesis Block", "0")
+# Generate genesis block
+def createGenesisBlock():
+  return Block(0, date.datetime.now(), {'proof-of-work': 9, 'transactions': []}, "0")
 
 class Blockchain:
   def __init__(self):
-    self.blocks = []
-    self.blocks.append(create_genesis_block())
+    self.__transactions = []
+    self.__blocks = []
+    self.__blocks.append(createGenesisBlock())
 
-  def toDict(self):
-    blocksArray = []
-    for i in range(len(self.blocks)):
-      block = self.blocks[i]
-      blocksArray.append(block.toDict())
-    return blocksArray
+  def addNewTransaction(self, sender, recipient, amount):
+    newTransaction = {'sender': sender,  'recipient': recipient, 'amount': amount}
+    self.__transactions.append(newTransaction)
+    return newTransaction
+
+  def proofOfWork(self):
+    lastProof = self.__lastProofOfWork
+    # Create a variable that we will use to find our next proof of work
+    incrementor = lastProof + 1
+    # Keep incrementing the incrementor until it's equal to a number divisible by 9 and the proof of work of the previous  block in the chain
+    while not (incrementor % 9 == 0 and incrementor % lastProof == 0):
+      incrementor += 1
+    # Once that number is found,  we can return it as a proof of our work
+    return incrementor
+
+  def addNewBlockWithProof(self, proof):
+    nBlockData = {'proof-of-work': proof, 'transactions': list(self.__transactions)}
+    self.__transactions = []
+    return self.__addNewBlockWithData(nBlockData)
+
+  def getBlocksAsDictArray(self):
+    return list(map(lambda block: block.toDict(), self.__blocks))
+
+  def __addNewBlockWithData(self, data):
+    lastBlock = self.__lastBlock
+    nBlock = Block(lastBlock.index + 1, date.datetime.now(), data, lastBlock.hash)
+    self.__blocks.append(nBlock)
+    return nBlock
+
+  @property
+  def __lastProofOfWork(self):
+    return self.__lastBlock.data['proof-of-work']
+
+  @property
+  def __lastBlock(self):
+    return self.__blocks[-1]
--- blockchain_py/src/main.py
+++ blockchain_py/src/main.py
@@ -7,20 +7,45 @@ import json
 from block import Block
 from blockchain import Blockchain
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 blockchain = Blockchain()
+# A completely random address of the owner of this node
+miner_address = "aaabbbcccd-random-miner-address-aaabbbcccd"
 
 @app.route('/')
 def hello():
     b = Block(0, time.ctime(), "Genesis Block", "0")
     return "%s, %s, %s, %s, %s" % (b.index, b.timestamp, b.data, b.hash, b.previous_hash)
 
-@app.route('/blocks')
-def get_blocks():
-    return json.dumps(blockchain.toDict())
+@app.route('/blocks', methods=['GET'])
+def getBlocks():
+    return json.dumps(blockchain.getBlocksAsDictArray())
+
+@app.route('/transactions', methods=['POST'])
+def newTransaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    return json.dumps(blockchain.addNewTransaction(values['sender'], values['recipient'], values['amount'])), 201
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # Get the last proof of work
+    # Find the proof of work for the current block being mined
+    # Note: The program will hang here until a new proof of work is found
+    proof = blockchain.proofOfWork()
+    # Once we find a valid proof of work, we know we can mine a block so we reward the miner by adding a transaction
+    blockchain.addNewTransaction("network", miner_address, 1)
+    # Now we can gather the data needed to create the new block, empty transaction list, now create the new block!
+    nBlock = blockchain.addNewBlockWithProof(proof)
+    return json.dumps(nBlock.toDict())
 
 if __name__ == "__main__":
     SERVICE_PORT = int(os.getenv("PORT", 5000))
